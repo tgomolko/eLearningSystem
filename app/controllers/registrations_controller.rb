74f1089dddb5a6508c5bd6c30d6ffdf07c502fcf
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-08-30 15:44:58 +0300

    log_in/sing_up fuctionalyty

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController 
+
+  private
+
+  def sign_up_params 
+    params.require(:user).permit(:name, :username, :role, :email, 
+                                 :password, :password_confirmation)
+  end
+
+  def acount_update_params 
+    params.require(:user).permit(:name, :username, :email, :role, 
+                                 :password, :password_confirmation, 
+                                 :current_password)
+  end
+
+end
