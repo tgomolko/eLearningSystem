9c23c7bd8ee57562696dfcb555209c8ebf636867
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-09-04 12:35:22 +0300

    fixed PR's issues and added locale translate in controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -29,11 +29,9 @@ class OrganizationsController < ApplicationController
 
     respond_to do |format|
       if @organization.save
-        format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
-        format.json { render :show, status: :created, location: @organization }
+        format.html { redirect_to @organization, notice: t(:org_created_successully) }
       else
         format.html { render :new }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,11 +41,9 @@ class OrganizationsController < ApplicationController
   def update
     respond_to do |format|
       if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
-        format.json { render :show, status: :ok, location: @organization }
+        format.html { redirect_to @organization, notice: t(:org_updated_successully) }
       else
         format.html { render :edit }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,19 +53,18 @@ class OrganizationsController < ApplicationController
   def destroy
     @organization.destroy
     respond_to do |format|
-      format.html { redirect_to organizations_url, notice: 'Organization was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to organizations_url, notice: t(:org_destroyed_successully) }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_organization
-      @organization = Organization.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_organization
+    @organization = Organization.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def organization_params
-      params.require(:organization).permit(:company_name, :description, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def organization_params
+    params.require(:organization).permit(:company_name, :description, :user_id)
+  end
 end
