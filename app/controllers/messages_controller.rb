f320708f5689c1b36d813d3bd797ca641c94c2e8
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-10-15 13:22:32 +0300

    EL9_code style im messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,7 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-  end
+  def new ; end
 
   def create
     recipients = User.where(id: params['recipients'])
