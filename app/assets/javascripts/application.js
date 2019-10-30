d53dc7c126837ee0add5d3f2dd07b7f13fefd550
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-10-30 18:47:59 +0300

    EL-14_added stycky navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -136,3 +136,23 @@ $(document).ready (function () {
 
   $('.chosen-it').chosen({width: '200px'});
 });
+
+$(document).ready(function() {
+  var stickyNavTop = $('.navbar').offset().top;
+
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > stickyNavTop) {
+        $('.navbar').addClass('sticky');
+    } else {
+        $('.navbar').removeClass('sticky');
+    }
+};
+
+stickyNav();
+
+$(window).scroll(function() {
+  stickyNav();
+  });
+});
