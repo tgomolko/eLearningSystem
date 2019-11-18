1fe77fffba199e8cee99cc7c98cc022e9e8cbf98
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-11-18 10:55:27 +0300

    EL-15_refactored metod import

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,11 +2,8 @@ class Email < ApplicationRecord
   validates :email, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
 
   def self.import(file)
-    return unless file.content_type == "text/csv"
     CSV.foreach(file.path, headers: true) do |row|
-      email = find_by(email: row["email"]) || new
-      email.attributes = row.to_hash
-      email.save
+      Email.find_or_create_by(email: row["email"])
     end
   end
 end
