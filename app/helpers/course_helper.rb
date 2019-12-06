cd8fe2cd573750f073638b3813cd7e53f1229991
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-12-06 18:04:51 +0300

    EL-28_changed course show UI

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -5,18 +5,19 @@ module CourseHelper
 
   def certificate_template(course)
     return unless course.attachment_pdf_file_name
+    content_tag(:h2, "Certificate template: ", class: "title is-5", style: "float:left")+
     link_to(course.attachment_pdf.url) do
-      content_tag(:span, content_tag(:i, "", class: "fa fa-file-pdf-o")).html_safe
+      content_tag(:span, content_tag(:i, " ", class: "fa fa-file-pdf-o")).html_safe
     end
   end
 
-  def created_by(course)
-    content_tag(:h1, "Created by: #{course.user.name} " + "#{time_ago_in_words(course.created_at)}" +
-    " ago", class: "subtitle is-5 is-pulled-right").html_safe
+  def created_at(course)
+    content_tag(:h3, "Created by: #{course.user.name} " + "#{time_ago_in_words(course.created_at)}" +
+    " ago", class: "subtitle is-6").html_safe
   end
 
   def main_course_image(course)
-    content_tag(:div, image_tag(course.image_url(:default)), class: "feature-image is-centered", style: "text-align: center;") if @course.image_url
+    content_tag(:div, image_tag(course.image_url(:default)), class: "feature-image") if @course.image_url
   end
 
   def course_action_buttons(course)
