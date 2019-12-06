3807b593b4da9cb8706f879681d6634b333509ac
Timofey Gomolko <t.i.m.a2009@mail.ru>
2019-12-06 14:05:08 +0300

    EL-26_set up fix width & root_path after

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
+  layout "welcome"
+
   def index
+    redirect_to user_dashboard_path and return if user_signed_in?
     @courses = Course.ready.first(5)
   end
 end
